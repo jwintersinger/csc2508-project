0f4a596bdae2832c8f6058cba9c8ef69bdba4320
Jeff Wintersinger <jeff.git@wintersinger.com>
2014-12-28 14:25:27 -0700

    Recreate table on each insert.

--- native/inserter.py
+++ native/inserter.py
@@ -5,12 +5,15 @@ import sys
 def main():
   conn = psycopg2.connect("dbname=postgres user=postgres")
   cur = conn.cursor()
-  cur.execute('DELETE FROM moviesj');
+
+  cur.execute('DROP TABLE IF EXISTS movies')
+  cur.execute('CREATE TABLE movies (doc JSONB)')
+  #cur.execute('DELETE FROM movies');
 
   recs = json.load(sys.stdin)
   for rec in recs:
     json_str = json.dumps(rec)
-    cur.execute('INSERT INTO moviesj (data) VALUES (%s)', (json_str,))
+    cur.execute('INSERT INTO movies (doc) VALUES (%s)', (json_str,))
 
   conn.commit()
   cur.close()
