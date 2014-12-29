2dc5fe6acd31a7801b0828ded376c5fd0b5deb3b
Jeff Wintersinger <jeff.git@wintersinger.com>
2014-12-29 10:08:12 -0700

    Add Postgres inserter.

---
+++ native/postgres_inserter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import psycopg2
+import sys
+
+def main():
+  conn = psycopg2.connect("dbname=postgres user=postgres")
+  cur = conn.cursor()
+
+  cur.execute('DROP TABLE IF EXISTS movies')
+  cur.execute('CREATE TABLE movies (doc JSONB)')
+
+  for line in sys.stdin:
+    cur.execute('INSERT INTO movies (doc) VALUES (%s)', (line,))
+
+  conn.commit()
+  cur.close()
+  conn.close()
+
+main()
