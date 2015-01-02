c206795c7ff4fc00c5aecab4ef11f22fa2b12b74
Jeff Wintersinger <jeff.git@wintersinger.com>
2015-01-02 12:00:26 -0500

    Benchmark inserts.

--- native/run.sh
+++ native/run.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
-set -euo pipefail
+set -uo pipefail
 
 function load_mongo {
+  mongo pants --eval 'db.movies.drop()' > /dev/null 2>&1
   input_file=$1
   mongo_insert_time=$(time (cat $input_file | mongoimport --drop --port 27017 --db pants --collection movies) 2>&1 1>/dev/null)
-  echo "Mongo insert time: $mongo_insert_time" && echo
+  echo "Mongo insert time: " $mongo_insert_time
 }
 
 function load_postgres_json {
+  dropdb -U postgres movies_json > /dev/null 2>&1
+  createdb -U postgres movies_json
   input_file=$1
   postgres_insert_time=$(time (cat $input_file | ./postgres_json_inserter.py) 2>&1 1>/dev/null)
-  echo "Postgres JSON insert time: $postgres_insert_time" && echo
+  echo "Postgres JSON insert time: " $postgres_insert_time
 }
 
 function load_postgres_relational {
+  dropdb -U postgres movies_relational > /dev/null 2>&1
+  createdb -U postgres movies_relational
   input_file=$1
   postgres_insert_time=$(time (cat $input_file | ./postgres_relational_inserter.py) 2>&1 1>/dev/null)
-  echo "Postgres relational insert time: $postgres_insert_time" && echo
+  echo "Postgres relational insert time: " $postgres_insert_time
 }
 
 function run_mongo {
@@ -31,16 +36,30 @@ function run_postgres_relational {
   ./postgres_relational_queries.py 1>/dev/null 2>results.postgres_relational_queries.csv
 }
 
-function main {
-  load_postgres_json $1 &
-  load_postgres_relational $1 &
-  load_mongo $1 &
+function insert_records {
+  load_postgres_json $1
+  load_postgres_relational $1
+  load_mongo $1
   wait
+}
 
-  run_postgres_json &
-  run_postgres_relational &
-  run_mongo &
+function benchmark_insert {
+  for foo in $(seq 10); do
+    insert_records $1
+  done
+}
+
+function benchmark_select {
+  insert_records $1
+  run_postgres_json
+  run_postgres_relational
+  run_mongo
   wait
+}
+
+function main {
+  #benchmark_insert
+  benchmark_select $1
 }
 
 main data/movies_100k.json
